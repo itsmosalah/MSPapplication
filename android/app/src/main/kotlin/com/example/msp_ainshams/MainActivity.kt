9694297ed8783fddb8425b5fc3dbd7e17206e117
AbdelrhmanY10 <abdelrhmanyasser123@gmail.com>
2021-10-16 20:08:33 +0200

    First Commit

---
+++ android/app/src/main/kotlin/com/example/msp_ainshams/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.msp_ainshams
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import'package:flutter/material.dart';
+
+
+void main(){
+
+
+}
+
+
+
+class MyApp extends StatelessWidget{
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+    );
+
+  }
+}
